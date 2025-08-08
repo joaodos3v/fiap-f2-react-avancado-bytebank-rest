7a71f5d179621d37eb4eaf0f0b5115ca1a13985e
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-08-08 10:20:23 -0300

    aula5-video1: list transaction types from API

--- src/components/TransactionForm/index.jsx
+++ src/components/TransactionForm/index.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react"
-import { Card, Form, Heading, Input, Label, Select } from "./styles"
+import { useEffect, useState } from "react"
+import http from "../../http"
 import { Button } from "../Button"
+import { Card, Form, Heading, Input, Label, Select } from "./styles"
 
 export const TransactionForm = () => {
 
     const [transactionType, setTransactionType] = useState('')
     const [transactionValue, setSetTransactionValue] = useState('')
+
+    const [transactionTypes, setTransactionTypes] = useState([])
 
     const createTransacion = (evt) => {
         evt.preventDefault()
@@ -15,27 +18,37 @@ export const TransactionForm = () => {
         })
     }
 
+    useEffect(() => {
+        http.get('transaction/types')
+            .then(response => setTransactionTypes(response.data))
+    }, []);
+
     return (
         <Card>
             <Heading>
                 Nova transação
             </Heading>
             <Form onSubmit={createTransacion}>
-                <Select     
-                    value={transactionType} 
+                <Select
+                    value={transactionType}
                     onChange={evt => setTransactionType(evt.target.value)}
                     required
                 >
                     <option value="" disabled hidden>
                         Selecione o tipo de transação
                     </option>
+                    {transactionTypes.map(type => (
+                        <option key={type.value} value={type.value}>
+                            {type.display}
+                        </option>
+                    ))}
                 </Select>
                 <div>
                     <Label>
                         Valor
                     </Label>
-                    <Input 
-                        placeholder="00,00" 
+                    <Input
+                        placeholder="00,00"
                         type="number"
                         value={transactionValue}
                         onChange={evt => setSetTransactionValue(evt.target.value)}
